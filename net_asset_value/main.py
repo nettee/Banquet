07fffbfad81c7261549876bd66c4e2ab4c5ad215
William <nettee.liu@gmail.com>
2018-04-22 16:37:13 +0800

    add ref stock price along with fund

--- net_asset_value/main.py
+++ net_asset_value/main.py
@@ -7,9 +7,10 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-import nav
+import fund
+import stock
 
-def stat(navs, buys, subscription_rate=0.001):
+def stat(navs, prices, buys, subscription_rate=0.001):
 
     buy_dates, buy_values = zip(*buys)
     buy_value = sum(buy_values)
@@ -17,14 +18,16 @@ def stat(navs, buys, subscription_rate=0.001):
     shares = []
 
     print('共买入{}次，买入总金额¥{:.2f}元'.format(len(buy_values), buy_value))
-    print('日期\t\t金额\t实际金额\t净值\t份额')
+    print('日期\t\t金额\t实际金额\t净值\t份额\t指数价格')
     for (bd, bv) in buys:
         rbv = bv - bv * subscription_rate
         nav = navs[bd]
         share = rbv / nav
         share = round(share, 2)
+        price = prices[bd]
+        print('{}\t¥{:.2f}\t¥{:.2f}\t\t{:.4f}\t{:.2f}\t{:.3f}'.format(bd, bv, rbv, nav, share, price[1]))
+
         shares.append(share)
-        print('{}\t¥{:.2f}\t¥{:.2f}\t\t{:.4f}\t{:.2f}'.format(bd, bv, rbv, nav, share))
 
     share = sum(shares)
     cost = buy_value / share
@@ -39,23 +42,31 @@ def stat(navs, buys, subscription_rate=0.001):
     print('持有收益：¥{:.2f}，持有收益率：{:.2%}'.format(holding_return, holding_return_rate))
 
     start_date, start_nav = navs.peekitem(0)
+    start_price = prices[start_date][1]
 
     all_data = []
     buy_data = []
+    rel_price_data = []
 
     for (i, (dat, nav)) in enumerate(navs.items()):
         ratio = (nav - start_nav) / start_nav
         all_data.append((i, dat, nav, ratio))
+        price = prices[dat][1]
+        rel_price = price * start_nav / start_price
+        rel_price_data.append((i, dat, rel_price))
         if dat in buy_dates:
             buy_data.append((i, dat, nav, ratio))
 
-    i, _, nav, _ = zip(*all_data)
-    plt.plot(i, nav, 'b')
+    i0, _, price = zip(*rel_price_data)
+    plt.plot(i0, price, color='#CCCC00')
+
+    i1, _, nav, _ = zip(*all_data)
+    plt.plot(i1, nav, 'b')
 
     i2, _, nav2, _ = zip(*buy_data)
     plt.scatter(i2, nav2, color='#B22222')
 
-    plt.hlines(cost, i[0], i[-1], color = 'c', linestyle = 'dashed')
+    plt.hlines(cost, i1[0], i1[-1], color = 'c', linestyle = 'dashed')
 
     plt.show()
 
@@ -63,6 +74,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('code')
+    parser.add_argument('index_code')
     parser.add_argument('buy_file')
 
     args = parser.parse_args()
@@ -80,5 +92,6 @@ if __name__ == '__main__':
     buy_dates, _ = zip(*buys)
     start_date = min(buy_dates)
 
-    navs = nav.nav(args.code, start_date)
-    stat(navs, buys)
+    navs = fund.nav(args.code, start_date)
+    prices = stock.prices(args.index_code, start_date=start_date)
+    stat(navs, prices, buys)
--- net_asset_value/stock.py
+++ net_asset_value/stock.py
@@ -3,6 +3,7 @@
 from datetime import datetime, timedelta
 
 import requests
+from sortedcontainers import SortedDict
 
 url_template = 'https://xueqiu.com/stock/forchartk/stocklist.json?symbol={code}&period={period}&type=normal&begin={begin}&end={end}'
 
@@ -12,23 +13,29 @@ headers = {
         'Cookie': 'aliyungf_tc=AQAAAJxYPH6WkgsAuxiYJKhg/iIP2U7J; xq_a_token=0d524219cf0dd2d0a4d48f15e36f37ef9ebcbee1; xq_a_token.sig=P0rdE1K6FJmvC2XfH5vucrIHsnw; xq_r_token=7095ce0c820e0a53c304a6ead234a6c6eca38488; xq_r_token.sig=xBQzKLc4EP4eZvezKxqxXNtB7K0; u=431524363212524; device_id=93d87730caedd0cebd0615467104c660; __utma=1.221323187.1524363212.1524363212.1524363212.1; __utmb=1.6.10.1524363212; __utmc=1; __utmz=1.1524363212.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); Hm_lvt_1db88642e346389874251b5a1eded6e3=1524363213; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1524363695',
 }
 
-def xueqiu_timestamp(time):
+def xueqiu_timestamp(date):
+    time = datetime(date.year, date.month, date.day)
     return round(time.timestamp() * 1000)
 
-def stock(code, period='1day', start_date=None):
+def prices(code, period='1day', start_date=None):
 
     if start_date is None:
         start_date = datetime.today() - timedelta(days=366)
+
+    ps = SortedDict()
 
     url = url_template.format(code=code, period=period, begin=xueqiu_timestamp(start_date), end=xueqiu_timestamp(datetime.today()))
     res = requests.get(url, headers=headers)
     json = res.json()
     chartlist = json['chartlist']
+
     for chart in chartlist:
         timestamp = int(chart['timestamp']) / 1000
         date = datetime.fromtimestamp(timestamp).date()
-        prices = [float(chart[s]) for s in ('open', 'close', 'high', 'low')]
-        print(date, prices)
+        price = [float(chart[s]) for s in ('open', 'close', 'high', 'low')]
+        ps[date] = price
+
+    return ps
 
 if __name__ == '__main__':
 
